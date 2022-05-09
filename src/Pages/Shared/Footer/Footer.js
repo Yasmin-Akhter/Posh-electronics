d3de8ab62c18e9254eb3aa7085823b526a2a4ac6
Yasmin Akhter <yasminbd89@yahoo.com>
2022-05-09 23:25:19 +0600

    updated

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
+    let v = new Date().getFullYear();
+
     return (
 
         <div>
 
             <div style={{ flexGrow: "1" }}></div>
-            <footer className='position-bottom mt-5'>
-                <h1>footer</h1>
+            <footer className='position-bottom mt-5 '>
+                <p className='mt-2'>Copyright@<span>{v}</span> posh electronics;</p>
             </footer>
         </div >
 
